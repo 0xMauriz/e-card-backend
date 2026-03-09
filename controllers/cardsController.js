aeb11cf5a4d031d165bfcfc36f06c18232bd08d9
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2026-03-09 14:53:52 +0100

    rotta store

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -2,11 +2,24 @@ const connection = require("../data/cardsData.js");
 
 // INDEX
 function index(req, res) {
-    const sql = "SELECT * FROM cardcommerce_db WHERE id=?"
+    const sql = "SELECT * FROM products"
 
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({error: "database query failed"})
         res.json(results);
+    })
+}
+
+// SHOW
+function show(req, res) {
+    const {id} = req.params
+
+    const sql = "SELECT * FROM products WHERE id=?"
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({error: "database query failed"})
+        if (results.length === 0) return res.status(404).json({error: "Page not found"})
+        res.json(results[0]);
     })
 }
 
